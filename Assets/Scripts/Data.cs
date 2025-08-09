de2556538f988ac0d9ae284869d532a2e2acca88
Costea Mihai <costea.mihai2002@yahoo.com>
2025-08-09 03:35:29 +0300

    Spin logic
    At every spin the game uses RNG to pick one number from 0 to 100 to stop each virtual reel
    
    There are 5 virtual reels each with 100 stops.
    The virtual reels are predefined and distinct.
    The weights of the virtual reels are the same and as fallows:
    25 tens
    12 jacks
    12 queens
    12 kings
    12 aces
    8 symbol_1
    8 symbol_2
    5 symbol_3
    4 wilds
    2 scatters

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -1,7 +1,7 @@
-using JetBrains.Annotations;
 using System;
 using System.Collections.Generic;
-using UnityEngine;
+
+using Random = UnityEngine.Random;
 
 [Serializable]
 public enum Symbols
@@ -26,7 +26,6 @@ public enum DoubleDownCards
     black
 };
 
-
 [Serializable]
 public enum BetAmounts
 {
@@ -50,11 +49,149 @@ public enum LinesAmount
     _10 = 10
 };
 
+
+
+public class Constants
+{
+    public const int NR_OF_STOPS = 100;
+
+    public const int NR_OF_REELS    = 5;    
+    public const int NR_OF_SQUARES  = 3;
+
+
+    public const LinesAmount MIN_NR_OF_LINES = LinesAmount._5;
+    public const LinesAmount MAX_NR_OF_LINES = LinesAmount._10;
+
+    public const BetAmounts MIN_BET = BetAmounts._10;
+    public const BetAmounts MAX_BET = BetAmounts._100;
+        
+};
+
+
+
+
+public class VirtualReels
+{
+    /*
+        25 tens
+        12 jacks
+        12 queens
+        12 kings
+        12 aces
+        8  symbol_1
+        8  symbol_2
+        5  symbol_3
+        4  wild
+        2  scatter
+
+    */
+
+    public static List<Symbols[]> reels => new List<Symbols[]>()
+    {
+        // Reel 1
+        new Symbols[Constants.NR_OF_STOPS]
+        {
+        Symbols.ten,        Symbols.king,       Symbols.queen,      Symbols.symbol_1,   Symbols.jack,       Symbols.ten,        Symbols.queen,      Symbols.scatter,    Symbols.queen,      Symbols.ten,
+        Symbols.ace,        Symbols.ten,        Symbols.ten,        Symbols.king,       Symbols.king,       Symbols.jack,       Symbols.symbol_2,   Symbols.jack,       Symbols.queen,      Symbols.scatter,
+        Symbols.ten,        Symbols.king,       Symbols.ten,        Symbols.symbol_3,   Symbols.ten,        Symbols.king,       Symbols.symbol_2,   Symbols.symbol_1,   Symbols.symbol_3,   Symbols.ten,
+        Symbols.ten,        Symbols.ace,        Symbols.ace,        Symbols.symbol_2,   Symbols.king,       Symbols.ten,        Symbols.symbol_3,   Symbols.symbol_1,   Symbols.ten,        Symbols.queen,
+        Symbols.symbol_1,   Symbols.symbol_2,   Symbols.ten,        Symbols.queen,      Symbols.ace,        Symbols.queen,      Symbols.jack,       Symbols.ten,        Symbols.ten,        Symbols.ten,
+        Symbols.jack,       Symbols.jack,       Symbols.ten,        Symbols.jack,       Symbols.queen,      Symbols.ten,        Symbols.jack,       Symbols.symbol_2,   Symbols.ten,        Symbols.jack,
+        Symbols.symbol_2,   Symbols.ace,        Symbols.king,       Symbols.king,       Symbols.ace,        Symbols.wild,       Symbols.king,       Symbols.symbol_1,   Symbols.ten,        Symbols.ace,
+        Symbols.ten,        Symbols.jack,       Symbols.ace,        Symbols.symbol_1,   Symbols.king,       Symbols.ace,        Symbols.wild,       Symbols.wild,       Symbols.ten,        Symbols.symbol_3,
+        Symbols.symbol_2,   Symbols.queen,      Symbols.symbol_2,   Symbols.wild,       Symbols.jack,       Symbols.queen,      Symbols.queen,      Symbols.ten,        Symbols.symbol_1,   Symbols.symbol_1,
+        Symbols.ace,        Symbols.ace,        Symbols.ace,        Symbols.king,       Symbols.king,       Symbols.jack,       Symbols.ten,        Symbols.ten,        Symbols.queen,      Symbols.symbol_3
+        },
+
+        // Reel2
+        new Symbols[Constants.NR_OF_STOPS] 
+        {
+        Symbols.ten,        Symbols.king,       Symbols.king,       Symbols.symbol_1,   Symbols.symbol_3,   Symbols.jack,       Symbols.wild,       Symbols.jack,       Symbols.queen,      Symbols.symbol_2,
+        Symbols.ace,        Symbols.queen,      Symbols.queen,      Symbols.ten,        Symbols.ten,        Symbols.king,       Symbols.ten,        Symbols.ace,        Symbols.wild,       Symbols.ace,
+        Symbols.jack,       Symbols.ten,        Symbols.symbol_3,   Symbols.queen,      Symbols.symbol_1,   Symbols.jack,       Symbols.symbol_1,   Symbols.ten,        Symbols.jack,       Symbols.ten,
+        Symbols.wild,       Symbols.king,       Symbols.king,       Symbols.queen,      Symbols.queen,      Symbols.ace,        Symbols.symbol_1,   Symbols.symbol_2,   Symbols.ten,        Symbols.symbol_1,
+        Symbols.ten,        Symbols.king,       Symbols.symbol_2,   Symbols.queen,      Symbols.ten,        Symbols.ten,        Symbols.symbol_2,   Symbols.king,       Symbols.jack,       Symbols.ace,
+        Symbols.scatter,    Symbols.ten,        Symbols.king,       Symbols.ten,        Symbols.ace,        Symbols.ace,        Symbols.king,       Symbols.symbol_2,   Symbols.ace,        Symbols.ace,
+        Symbols.ten,        Symbols.queen,      Symbols.symbol_2,   Symbols.ten,        Symbols.queen,      Symbols.jack,       Symbols.ace,        Symbols.jack,       Symbols.symbol_3,   Symbols.queen,
+        Symbols.ten,        Symbols.symbol_2,   Symbols.ten,        Symbols.wild,       Symbols.king,       Symbols.ten,        Symbols.ten,        Symbols.king,       Symbols.ten,        Symbols.ten,
+        Symbols.scatter,    Symbols.symbol_1,   Symbols.queen,      Symbols.symbol_3,   Symbols.jack,       Symbols.ten,        Symbols.symbol_2,   Symbols.jack,       Symbols.symbol_1,   Symbols.symbol_1,
+        Symbols.queen,      Symbols.ten,        Symbols.ace,        Symbols.jack,       Symbols.ace,        Symbols.king,       Symbols.ten,        Symbols.jack,       Symbols.ten,        Symbols.symbol_3
+        },
+
+        // Reel 3
+        new Symbols[Constants.NR_OF_STOPS]
+        {
+        Symbols.ten,        Symbols.queen,      Symbols.ten,        Symbols.ace,        Symbols.ten,        Symbols.ten,        Symbols.ten,        Symbols.symbol_2,   Symbols.ten,        Symbols.scatter,
+        Symbols.symbol_1,   Symbols.ten,        Symbols.jack,       Symbols.ace,        Symbols.jack,       Symbols.symbol_2,   Symbols.jack,       Symbols.ten,        Symbols.symbol_1,   Symbols.jack,
+        Symbols.queen,      Symbols.ten,        Symbols.symbol_2,   Symbols.ten,        Symbols.ace,        Symbols.ten,        Symbols.ten,        Symbols.jack,       Symbols.ace,        Symbols.ten,
+        Symbols.ace,        Symbols.queen,      Symbols.wild,       Symbols.king,       Symbols.ten,        Symbols.ace,        Symbols.ten,        Symbols.queen,      Symbols.jack,       Symbols.symbol_3,
+        Symbols.symbol_2,   Symbols.wild,       Symbols.ten,        Symbols.ten,        Symbols.scatter,    Symbols.queen,      Symbols.symbol_2,   Symbols.queen,      Symbols.jack,       Symbols.ten,
+        Symbols.symbol_2,   Symbols.symbol_3,   Symbols.king,       Symbols.symbol_1,   Symbols.queen,      Symbols.jack,       Symbols.ten,        Symbols.king,       Symbols.ace,        Symbols.queen,
+        Symbols.ten,        Symbols.ten,        Symbols.symbol_1,   Symbols.symbol_1,   Symbols.symbol_3,   Symbols.symbol_1,   Symbols.symbol_3,   Symbols.wild,       Symbols.jack,       Symbols.ace,
+        Symbols.ten,        Symbols.jack,       Symbols.queen,      Symbols.symbol_3,   Symbols.king,       Symbols.symbol_1,   Symbols.ten,        Symbols.king,       Symbols.ace,        Symbols.king,
+        Symbols.king,       Symbols.symbol_2,   Symbols.king,       Symbols.wild,       Symbols.jack,       Symbols.king,       Symbols.symbol_2,   Symbols.queen,      Symbols.ten,        Symbols.queen,
+        Symbols.jack,       Symbols.ace,        Symbols.ace,        Symbols.ace,        Symbols.symbol_1,   Symbols.ten,        Symbols.king,       Symbols.king,       Symbols.king,       Symbols.queen
+        },
+
+        // Reel 4
+        new Symbols[Constants.NR_OF_STOPS]
+        {
+        Symbols.ace,        Symbols.king,       Symbols.ace,        Symbols.ten,        Symbols.ace,        Symbols.queen,      Symbols.symbol_2,   Symbols.ten,        Symbols.symbol_3,   Symbols.ten,
+        Symbols.symbol_3,   Symbols.king,       Symbols.ace,        Symbols.ten,        Symbols.queen,      Symbols.king,       Symbols.ace,        Symbols.ten,        Symbols.jack,       Symbols.jack,
+        Symbols.king,       Symbols.king,       Symbols.symbol_3,   Symbols.ten,        Symbols.ace,        Symbols.queen,      Symbols.ace,        Symbols.symbol_3,   Symbols.wild,       Symbols.ten,
+        Symbols.king,       Symbols.king,       Symbols.symbol_1,   Symbols.symbol_1,   Symbols.ten,        Symbols.ten,        Symbols.symbol_1,   Symbols.symbol_2,   Symbols.symbol_1,   Symbols.symbol_2,
+        Symbols.wild,       Symbols.symbol_2,   Symbols.symbol_2,   Symbols.symbol_1,   Symbols.ace,        Symbols.ace,        Symbols.king,       Symbols.ten,        Symbols.queen,      Symbols.ten,
+        Symbols.jack,       Symbols.jack,       Symbols.queen,      Symbols.symbol_1,   Symbols.ten,        Symbols.symbol_2,   Symbols.king,       Symbols.king,       Symbols.ten,        Symbols.ten,
+        Symbols.ten,        Symbols.symbol_1,   Symbols.ace,        Symbols.ten,        Symbols.ten,        Symbols.ten,        Symbols.ten,        Symbols.wild,       Symbols.scatter,    Symbols.ten,
+        Symbols.ace,        Symbols.queen,      Symbols.jack,       Symbols.symbol_2,   Symbols.queen,      Symbols.jack,       Symbols.wild,       Symbols.queen,      Symbols.jack,       Symbols.king,
+        Symbols.symbol_2,   Symbols.ten,        Symbols.jack,       Symbols.ten,        Symbols.queen,      Symbols.king,       Symbols.symbol_1,   Symbols.jack,       Symbols.ten,        Symbols.jack,
+        Symbols.symbol_3,   Symbols.scatter,    Symbols.queen,      Symbols.ten,        Symbols.ace,        Symbols.ten,        Symbols.jack,       Symbols.queen,      Symbols.jack,       Symbols.queen
+        },
+
+        // Reel 5
+        new Symbols[Constants.NR_OF_STOPS]
+        {
+        Symbols.symbol_2,   Symbols.symbol_2,   Symbols.jack,       Symbols.symbol_2,   Symbols.symbol_3,   Symbols.king,       Symbols.ten,        Symbols.scatter,    Symbols.king,       Symbols.king,
+        Symbols.jack,       Symbols.jack,       Symbols.symbol_1,   Symbols.ace,        Symbols.symbol_2,   Symbols.ace,        Symbols.wild,       Symbols.king,       Symbols.ten,        Symbols.queen,
+        Symbols.symbol_2,   Symbols.ace,        Symbols.ace,        Symbols.ten,        Symbols.jack,       Symbols.ten,        Symbols.king,       Symbols.ten,        Symbols.ace,        Symbols.ten,
+        Symbols.ten,        Symbols.ten,        Symbols.ten,        Symbols.symbol_1,   Symbols.jack,       Symbols.ace,        Symbols.symbol_3,   Symbols.ace,        Symbols.queen,      Symbols.queen,
+        Symbols.symbol_1,   Symbols.jack,       Symbols.ten,        Symbols.symbol_3,   Symbols.queen,      Symbols.symbol_1,   Symbols.king,       Symbols.king,       Symbols.king,       Symbols.symbol_2,
+        Symbols.ten,        Symbols.queen,      Symbols.queen,      Symbols.queen,      Symbols.ten,        Symbols.king,       Symbols.ten,        Symbols.ten,        Symbols.ten,        Symbols.jack,
+        Symbols.symbol_1,   Symbols.wild,       Symbols.ten,        Symbols.wild,       Symbols.symbol_1,   Symbols.symbol_3,   Symbols.symbol_1,   Symbols.ace,        Symbols.ace,        Symbols.king,
+        Symbols.ace,        Symbols.king,       Symbols.wild,       Symbols.ace,        Symbols.queen,      Symbols.ten,        Symbols.ten,        Symbols.ten,        Symbols.jack,       Symbols.jack,
+        Symbols.symbol_1,   Symbols.symbol_2,   Symbols.king,       Symbols.ten,        Symbols.jack,       Symbols.symbol_2,   Symbols.ten,        Symbols.jack,       Symbols.scatter,    Symbols.ten,
+        Symbols.queen,      Symbols.ace,        Symbols.queen,      Symbols.ten,        Symbols.symbol_3,   Symbols.ten,        Symbols.queen,      Symbols.ten,        Symbols.jack,       Symbols.queen
+        }
+
+    };
+}
+
 public class Reel
 {
-    public Symbols[] squares { get; private set; }
+    private Symbols[] squares = new Symbols[Constants.NR_OF_SQUARES];
+
+    private int index;
+
+    
+    public Reel(int _index) => index = _index;
+    public Symbols[] Get() => squares;
+    
+    public void Stop()
+    {
+        int center = Random.Range(0, Constants.NR_OF_STOPS);
+
+        int top = (center == 0) ? Constants.NR_OF_STOPS - 1 : center - 1;
+        int bottom = (center == Constants.NR_OF_STOPS - 1) ? 0 : center + 1;
+
+        Set(VirtualReels.reels[index][top], VirtualReels.reels[index][center], VirtualReels.reels[index][bottom]);
+    }
+    private void Set(Symbols top, Symbols center, Symbols bottom)
+    {
+        squares[0] = top;
+        squares[1] = center;
+        squares[2] = bottom;
+    }
 }
-public static class Constants
 {
     public static int NR_OF_REELS => 5;    
     public static int NR_OF_SQUARES => 3;
--- Assets/Scripts/MNG_Base.cs
+++ Assets/Scripts/MNG_Base.cs
@@ -6,8 +6,12 @@ public class MNG_Base : MonoBehaviour
 {
     public static MNG_Base singleton;
 
+    private Reel[] reels = new Reel[Constants.NR_OF_REELS] { new Reel(0), new Reel(1), new Reel(2), new Reel(3), new Reel(4) };
+
+
+
+
     private void Awake() => Initialize();
-    
     private void Initialize()
     {
         if (singleton != null)
@@ -16,11 +20,12 @@ public class MNG_Base : MonoBehaviour
         singleton = this;
     }
 
-
     public void Spin()
     {
         MNG_Credits.singleton.RemoveCredit(MNG_Controls.singleton.totalBet);
-        MNG_Credits.singleton.AddPayout(5);
+        MNG_Credits.singleton.AddPayout(MNG_Payment.singleton.CalculateTotalPayout());
+
+        SetReels();
     }
 
     public void TakeWin()
@@ -28,8 +33,18 @@ public class MNG_Base : MonoBehaviour
         MNG_Credits.singleton.AddCredit(MNG_Credits.singleton.payout);
         MNG_Credits.singleton.RemovePayout();
     }
-    private void CheckForBonusGames()
-    {
 
+    private void SetReels()
+    {
+        foreach (Reel reel in reels)
+            reel.Stop();
+
+        foreach (Reel reel in reels)
+        {
+            foreach (Symbols symbol in reel.Get())
+                Debug.Log(symbol);
+
+            Debug.Log("\n");
+        }
     }
 }
